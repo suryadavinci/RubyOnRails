b856772deaeef31095ba0fcc9a779e19c08de220
Surya DaVinci <suryadavinci@gmail.com>
2017-04-27 23:18:41 +0530

    removing spblog

--- spblog_old/app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ApplicationRecord
-	belongs_to :user
-	has_many :comments
-	validates :title, presence: true, length: {maximum: 50}
-	validates :post, presence: true
-end
--- spblog_old/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-	has_many :articles
-	validates :password, presence: true, length: {minimum: 8}
-	validates :email, presence: true	
-end
